1656954b08402c3dddcccc561ecdbfed4593ad47
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2014-10-11 03:30:37 +0200

    added misc.profile()

---
+++ deeppy/misc/profile.py
@@ -0,0 +1,26 @@
+import time
+import cudarray as ca
+from ..helpers import one_hot_encode, one_hot_decode
+
+
+def profile(neuralnet, X, Y, reps=100):
+    Y_one_hot = one_hot_encode(Y)
+    neuralnet._setup(X, Y_one_hot)
+    layer_input = ca.array(X)
+    for layer_idx, layer in enumerate(neuralnet.layers[:-1]):
+        start_time = time.time()
+        for _ in range(reps):
+            layer.fprop(layer_input, 'train')
+        end_time = time.time()
+        fprop_duration = float(end_time - start_time)/reps
+
+        layer_output = layer.fprop(layer_input, 'train')
+        start_time = time.time()
+        for _ in range(reps):
+            layer.bprop(layer_output)
+        end_time = time.time()
+        bprop_duration = float(end_time - start_time)/reps
+
+        print('%s:   \tfprop: %.6f s \t bprop: %.6f s'
+              % (layer.name, fprop_duration, bprop_duration))
+        layer_input = layer_output
