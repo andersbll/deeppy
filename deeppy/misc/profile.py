8cb0fa2818bd35d84fd1a60f165915cbd6ebf035
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2014-10-13 12:42:01 +0200

    misc.profile(): cleanup and added total duration

--- deeppy/misc/profile.py
+++ deeppy/misc/profile.py
@@ -2,23 +2,39 @@ import time
 import cudarray as ca
 
 
-def profile(neuralnet, X, Y, reps=100):
+def avg_running_time(fun, reps):
+    # Memory allocation forces GPU synchronization
+    ca.empty(1)
+    start_time = time.time()
+    for _ in range(reps):
+        fun()
+    ca.empty(1)
+    duration = time.time() - start_time
+    return float(time.time() - start_time) / reps
+
+
+def profile(neuralnet, X, Y, reps=50):
     neuralnet._setup(X, Y)
-    layer_input = ca.array(X)
+    X = ca.array(X)
+    Y = ca.array(Y)
+    layer_input = X
+    total_duration = 0
     for layer_idx, layer in enumerate(neuralnet.layers[:-1]):
-        start_time = time.time()
-        for _ in range(reps):
+        def fprop():
             layer.fprop(layer_input, 'train')
-        end_time = time.time()
-        fprop_duration = float(end_time - start_time)/reps
-
+        fprop_duration = avg_running_time(fprop, reps)
         layer_output = layer.fprop(layer_input, 'train')
-        start_time = time.time()
-        for _ in range(reps):
-            layer.bprop(layer_output)
-        end_time = time.time()
-        bprop_duration = float(end_time - start_time)/reps
 
-        print('%s:   \tfprop: %.6f s \t bprop: %.6f s'
+        def bprop():
+            layer.bprop(layer_output)
+        bprop_duration = avg_running_time(bprop, reps)
+        print('%s:   \tfprop(): %.6f s \t bprop(): %.6f s'
               % (layer.name, fprop_duration, bprop_duration))
         layer_input = layer_output
+        total_duration += fprop_duration + bprop_duration
+    print('total_duration: %.6f s' % total_duration)
+
+    def nn_bprop():
+        neuralnet._bprop(X, Y)
+    nn_bprop_duration = avg_running_time(nn_bprop, reps)
+    print('neuralnet._bprop(): %.6f s' % nn_bprop_duration)
