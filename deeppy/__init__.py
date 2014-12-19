4cc5d64c9e316edf593d9ad75882c834a0b22199
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2014-12-19 20:22:11 +0100

    moved logger configuration to base This avoids polluting the deeppy namespace.

--- deeppy/__init__.py
+++ deeppy/__init__.py
@@ -1,9 +1,3 @@
-import logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(levelname)-8s %(message)s',
-)
-
 from . import datasets
 from . import misc
 from .base import (
--- deeppy/base.py
+++ deeppy/base.py
@@ -1,9 +1,13 @@
 import numpy as np
 import cudarray as ca
+import logging
 from .fillers import filler, Filler
 
-import logging
 logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(levelname)-8s %(message)s',
+)
 
 
 bool_ = ca.bool_
