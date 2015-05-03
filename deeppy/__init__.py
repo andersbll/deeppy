0dba67bc8578346ad5e3a9d9b11cce23adcfe98f
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2015-05-03 13:09:02 +0200

    cleanup

--- deeppy/__init__.py
+++ deeppy/__init__.py
@@ -1,59 +1,21 @@
-__version__ = '0.1.dev'
-
 from . import datasets
 from . import misc
-from .autoencoder import (
-    Autoencoder,
-    DenoisingAutoencoder,
-    StackedAutoencoder,
-)
-from .base import (
-    bool_,
-    int_,
-    float_,
-)
-from .input import (
-    Input,
-    SupervisedInput,
-)
+from .autoencoder import Autoencoder, DenoisingAutoencoder, StackedAutoencoder
+from .base import bool_, int_, float_
+from .input import Input, SupervisedInput
 from .feed_forward import (
-    NeuralNetwork,
-    FullyConnected,
-    Activation,
-    MultinomialLogReg,
-    Dropout,
-    DropoutFullyConnected,
-    Convolutional,
-    Flatten,
-    Pool,
-    LocalContrastNormalization,
-    LocalResponseNormalization,
+    NeuralNetwork, FullyConnected, Activation, MultinomialLogReg, Dropout,
+    DropoutFullyConnected, Convolutional, Flatten, Pool,
+    LocalContrastNormalization, LocalResponseNormalization,
 )
 from .fillers import (
-    AutoFiller,
-    CopyFiller,
-    ConstantFiller,
-    NormalFiller,
-    UniformFiller,
+    AutoFiller, CopyFiller, ConstantFiller, NormalFiller, UniformFiller,
 )
-from .parameter import (
-    Parameter,
-)
-
-from .preprocess import (
-    StandardScaler,
-    UniformScaler,
-)
+from .parameter import Parameter
+from .preprocess import StandardScaler, UniformScaler
 from .siamese import (
-    ContrastiveLoss,
-    SiameseNetwork,
-    SiameseInput,
-    SupervisedSiameseInput,
+    ContrastiveLoss, SiameseNetwork, SiameseInput, SupervisedSiameseInput,
 )
-from .trainers.learning_rules import (
-    Momentum,
-    RMSProp,
-)
-from .trainers.sgd import (
-    StochasticGradientDescent,
-)
+from .trainers import Momentum, RMSProp, StochasticGradientDescent
+
+__version__ = '0.1.dev'
