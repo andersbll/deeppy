dc54068d328eaf8b12507d6126a82bb5ae5c1840
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2015-05-12 19:31:51 +0200

    revised autoencoders

--- deeppy/autoencoder/autoencoder.py
+++ deeppy/autoencoder/autoencoder.py
@@ -1,25 +1,13 @@
+import numpy as np
 import cudarray as ca
 from ..feedforward.layers import Activation, FullyConnected
 from ..feedforward.loss import Loss
 from ..base import Model, PickleMixin
+from ..input import Input
 from ..parameter import Parameter
 
 
-class AutoencoderBase(object):
-    def encode(x):
-        raise NotImplementedError()
-
-    def decode(y):
-        raise NotImplementedError()
-
-    def encode_bprop(y):
-        raise NotImplementedError()
-
-    def decode_bprop(x):
-        raise NotImplementedError()
-
-
-class Autoencoder(Model, AutoencoderBase, PickleMixin):
+class Autoencoder(Model, PickleMixin):
     def __init__(self, n_out, weights, bias=0.0, activation='sigmoid',
                  loss='bce'):
         self.name = 'autoenc'
@@ -68,10 +56,16 @@ class Autoencoder(Model, AutoencoderBase, PickleMixin):
         y = ca.dot(x, self.W.array) + self.b.array
         return self.activation.fprop(y, '')
 
-    def decode(self, y_prime):
-        self._tmp_last_y_prime = y_prime
-        x_prime = ca.dot(y_prime, self.W.array.T) + self.b_prime.array
-        return self.activation_decode.fprop(x_prime, '')
+    def decode(self, y):
+        self._tmp_last_y = y
+        x = ca.dot(y, self.W.array.T) + self.b_prime.array
+        return self.activation_decode.fprop(x, '')
+
+    def decode_bprop(self, x_grad):
+        x_grad = self.activation_decode.bprop(x_grad)
+        ca.dot(x_grad.T, self._tmp_last_y, out=self.W.grad_array)
+        ca.sum(x_grad, axis=0, out=self.b_prime.grad_array)
+        return ca.dot(x_grad, self.W.array)
 
     def encode_bprop(self, y_grad):
         y_grad = self.activation.bprop(y_grad)
@@ -82,12 +76,6 @@ class Autoencoder(Model, AutoencoderBase, PickleMixin):
         ca.sum(y_grad, axis=0, out=self.b.grad_array)
         return ca.dot(y_grad, self.W.array.T)
 
-    def decode_bprop(self, x_prime_grad):
-        x_prime_grad = self.activation_decode.bprop(x_prime_grad)
-        ca.dot(x_prime_grad.T, self._tmp_last_y_prime, out=self.W.grad_array)
-        ca.sum(x_prime_grad, axis=0, out=self.b_prime.grad_array)
-        return ca.dot(x_prime_grad, self.W.array)
-
     def _update(self, x):
         y_prime = self.encode(x)
         x_prime = self.decode(y_prime)
@@ -95,6 +83,37 @@ class Autoencoder(Model, AutoencoderBase, PickleMixin):
         y_grad = self.decode_bprop(x_prime_grad)
         self.encode_bprop(y_grad)
         return self.loss.loss(x, x_prime)
+
+    def _reconstruct_batch(self, x):
+        y = self.encode(x)
+        return self.decode(y)
+
+    def reconstruct(self, input):
+        """ Returns the reconstructed input. """
+        input = Input.from_any(input)
+        x_prime = np.empty(input.x.shape)
+        offset = 0
+        for x_batch in input.batches('test'):
+            x_prime_batch = np.array(self._reconstruct_batch(x_batch))
+            batch_size = x_prime_batch.shape[0]
+            x_prime[offset:offset+batch_size, ...] = x_prime_batch
+            offset += batch_size
+        return x_prime
+
+    def _embed_batch(self, x):
+        return self.encode(x)
+
+    def embed(self, input):
+        """ Returns the embedding of the input. """
+        input = Input.from_any(input)
+        y = np.empty(self.output_shape(input.x.shape))
+        offset = 0
+        for x_batch in input.batches('test'):
+            y_batch = np.array(self._embed_batch(x_batch))
+            batch_size = y_batch.shape[0]
+            y[offset:offset+batch_size, ...] = y_batch
+            offset += batch_size
+        return y
 
     def feedforward_layers(self):
         return [FullyConnected(self.n_out, self.W.array, self.b.array),
--- deeppy/autoencoder/stacked_autoencoder.py
+++ deeppy/autoencoder/stacked_autoencoder.py
@@ -1,7 +1,7 @@
 import itertools
-from ..base import Model, ParamMixin
+from ..base import ParamMixin
 from ..feedforward.loss import Loss
-from .autoencoder import AutoencoderBase
+from .autoencoder import Autoencoder
 
 
 class InputWrap(object):
@@ -16,7 +16,7 @@ class InputWrap(object):
         return getattr(self.ae, attr)
 
 
-class StackedAutoencoderLayer(AutoencoderBase):
+class StackedAutoencoderLayer(Autoencoder):
     def __init__(self, ae, prev_layers):
         self.ae = ae
         self.prev_layers = prev_layers
@@ -34,20 +34,32 @@ class StackedAutoencoderLayer(AutoencoderBase):
         self._initialized = True
 
     def _update(self, x):
-        for layer in self.prev_layers:
-            x = layer.encode(x)
+        for ae in self.prev_layers:
+            x = ae.encode(x)
         return self.ae._update(x)
 
+    def _reconstruct_batch(self, x):
+        for ae in self.prev_layers:
+            x = ae.encode(x)
+        y = self.ae.encode(x)
+        x_prime = self.ae.decode(y)
+        for ae in reversed(self.prev_layers):
+            x_prime = ae.decode(x_prime)
+        return x_prime
+
+    def _embed_batch(self, x):
+        for ae in self.prev_layers:
+            x = ae.encode(x)
+        return self.ae.encode(x)
+
     def __getattr__(self, attr):
-        # Wrap Autoencoder methods
-        # abll: this is a hack. I thin it would be better to do plain
-        # inheritance from Autoencoder
+        # Wrap non-overriden Autoencoder attributes
         if attr in self.__dict__:
             return getattr(self, attr)
         return getattr(self.ae, attr)
 
 
-class StackedAutoencoder(Model, AutoencoderBase):
+class StackedAutoencoder(Autoencoder):
     def __init__(self, layers, loss='bce'):
         self._initialized = False
         self.layers = layers
@@ -65,36 +77,41 @@ class StackedAutoencoder(Model, AutoencoderBase):
 
     @property
     def _params(self):
-        all_params = [layer._params for layer in self.layers
-                      if isinstance(layer, ParamMixin)]
+        all_params = [ae._params for ae in self.layers
+                      if isinstance(ae, ParamMixin)]
         # Concatenate lists in list
         return list(itertools.chain.from_iterable(all_params))
 
-    def _update(self, x):
-        y = x
+    def encode(self, x):
         for ae in self.layers:
-            y = ae.encode(y)
-        z = y
+            x = ae.encode(x)
+        return x
+
+    def decode(self, y):
         for ae in reversed(self.layers):
-            z = ae.decode(z)
-        y_grad = self.loss.grad(x, z)
+            y = ae.decode(y)
+        return y
+
+    def decode_bprop(self, x_grad):
         for ae in self.layers:
-            y_grad = ae.decode_bprop(y_grad)
-        x_grad = y_grad
+            x_grad = ae.decode_bprop(x_grad)
+        return x_grad
+
+    def encode_bprop(self, y_grad):
         for ae in reversed(self.layers):
-            x_grad = ae.encode_bprop(x_grad)
-        return self.loss.loss(x, z)
+            y_grad = ae.encode_bprop(y_grad)
+        return y_grad
 
     def _output_shape(self, input_shape):
         next_shape = input_shape
-        for layer in self.layers:
-            next_shape = layer.output_shape(next_shape)
+        for ae in self.layers:
+            next_shape = ae.output_shape(next_shape)
         return next_shape
-
-    def ae_models(self):
-        for i, ae in enumerate(self.layers):
-            yield StackedAutoencoderLayer(ae, self.layers[:i])
 
     def feedforward_layers(self):
         feedforward_layers = [ae.feedforward_layers() for ae in self.layers]
         return list(itertools.chain.from_iterable(feedforward_layers))
+
+    def ae_models(self):
+        for i, ae in enumerate(self.layers):
+            yield StackedAutoencoderLayer(ae, self.layers[:i])
