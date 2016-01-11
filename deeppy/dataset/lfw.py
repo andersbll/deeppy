b55dee843fb2d9b9c8f133d4f929fcd468906aa9
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2016-01-11 13:19:32 +0100

    add train/val/test splits

--- deeppy/dataset/lfw.py
+++ deeppy/dataset/lfw.py
@@ -21,6 +21,36 @@ _URLS = {
     ),
 }
 
+_PAIR_SPLIT_URLS = {
+    'train': (
+        'http://vis-www.cs.umass.edu/lfw/pairsDevTrain.txt',
+        '4f27cbf15b2da4a85c1907eb4181ad21',
+    ),
+    'val': (
+        'http://vis-www.cs.umass.edu/lfw/pairsDevTest.txt',
+        '5132f7440eb68cf58910c8a45a2ac10b',
+    ),
+    'test': (
+        'http://vis-www.cs.umass.edu/lfw/pairs.txt',
+        '9f1ba174e4e1c508ff7cdf10ac338a7d',
+    ),
+}
+
+_PEOPLE_SPLIT_URLS = {
+    'train': (
+        'http://vis-www.cs.umass.edu/lfw/peopleDevTrain.txt',
+        '54eaac34beb6d042ed3a7d883e247a21',
+    ),
+    'val': (
+        'http://vis-www.cs.umass.edu/lfw/peopleDevTest.txt',
+        'e4bf5be0a43b5dcd9dc5ccfcb8fb19c5',
+    ),
+    'test': (
+        'http://vis-www.cs.umass.edu/lfw/people.txt',
+        '450f0863dd89e85e73936a6d71a3474b',
+    ),
+}
+
 
 class LFW(object):
     '''
@@ -42,10 +72,9 @@ class LFW(object):
         self.data_dir = os.path.join(data_root, self.name)
         self._npz_path = os.path.join(self.data_dir, self.name+'.npz')
         self._install()
-        self.imgs, self.names_idx, self.names = self._load()
-
-    def arrays(self):
-        return self.imgs, self.names_idx, self.names
+        self.people_splits = self._load()
+        (self.imgs, self.index, self.people_splits,
+         self.pair_splits) = self._load()
 
     def _install(self):
         checkpoint_file = os.path.join(self.data_dir, '__install_check')
@@ -58,39 +87,99 @@ class LFW(object):
             filepath = download(url, self.data_dir)
             if md5 != checksum(filepath, method='md5'):
                 raise RuntimeError('Checksum mismatch for %s.' % url)
-
             log.info('Unpacking %s', filepath)
             archive_extract(filepath, self.data_dir)
             extract_dir = os.path.splitext(os.path.split(filepath)[1])[0]
             extract_dir = os.path.join(self.data_dir, extract_dir)
 
             log.info('Converting images to NumPy arrays')
-            name_dict = {}
+            index = {}
             imgs = []
             img_idx = 0
-            for root, dirs, files in os.walk(extract_dir):
-                for filename in files:
-                    _, ext = os.path.splitext(filename)
-                    if ext.lower() != '.jpg':
-                        continue
-                    filepath = os.path.join(root, filename)
+            for person_id in sorted(os.listdir(extract_dir)):
+                person_dir = os.path.join(extract_dir, person_id)
+                img_no = 0
+                for filename in sorted(os.listdir(person_dir)):
+                    if int(filename[-8:-4]) - 1 != img_no:
+                        raise ValueError('Unexpected file: %s' % filename)
+                    filepath = os.path.join(person_dir, filename)
                     imgs.append(np.array(Image.open(filepath)))
-                    _, name = os.path.split(root)
-                    if name not in name_dict:
-                        name_dict[name] = []
-                    name_dict[name].append(img_idx)
+                    if person_id not in index:
+                        index[person_id] = []
+                    index[person_id].append(img_idx)
                     img_idx += 1
+                    img_no += 1
             imgs = np.array(imgs)
-            names = sorted(name_dict.keys())
-            names_idx = np.empty(len(imgs))
-            for name_idx, name in enumerate(names):
-                for img_idx in name_dict[name]:
-                    names_idx[img_idx] = name_idx
+
+            log.info('Downloading splits')
+            people_splits = {}
+            for split_name, (url, md5) in _PEOPLE_SPLIT_URLS.items():
+                filepath = download(url, self.data_dir)
+                if md5 != checksum(filepath, method='md5'):
+                    raise RuntimeError('Checksum mismatch for %s.' % url)
+                sets = []
+                set_sizes = []
+                current_set = -1
+                with open(filepath, 'r') as f:
+                    if 'test' in split_name:
+                        f.readline()
+                    for line in f:
+                        fields = line.strip().split('\t')
+                        if len(fields) == 2:
+                            (name, n_imgs) = fields
+                            sets[current_set].append(name)
+                        else:
+                            set_sizes.append(int(fields[0]))
+                            sets.append([])
+                            current_set += 1
+                if set_sizes != [len(s) for s in sets]:
+                    raise ValueError('Wrong # of images in sets.')
+                if len(sets) == 1:
+                    sets = sets[0]
+                people_splits[split_name] = sets
+
+            pair_splits = {}
+            for split_name, (url, md5) in _PAIR_SPLIT_URLS.items():
+                filepath = download(url, self.data_dir)
+                if md5 != checksum(filepath, method='md5'):
+                    raise RuntimeError('Checksum mismatch for %s.' % url)
+                sets = []
+                set_sizes = []
+                current_set = -1
+                with open(filepath, 'r') as f:
+                    for line in f:
+                        fields = line.strip().split('\t')
+                        if len(fields) == 1:
+                            set_sizes.append(int(fields[0])*2)
+                            sets.append([])
+                            continue
+                        elif len(fields) == 2:
+                            for _ in range(int(fields[0])):
+                                set_sizes.append(int(fields[1])*2)
+                                sets.append([])
+                            current_set += 1
+                            continue
+                        elif len(fields) == 3:
+                            (name0, img_idx0, img_idx1) = fields
+                            name1 = name0
+                        elif len(fields) == 4:
+                            (name0, img_idx0, name1, img_idx1) = fields
+                        sets[current_set].append((name0, int(img_idx0), name1,
+                                                  int(img_idx1)))
+                        if len(sets[current_set]) == set_sizes[current_set]:
+                            current_set += 1
+                if set_sizes != [len(s) for s in sets]:
+                    raise ValueError('Wrong # of images in sets.')
+                if len(sets) == 1:
+                    sets = sets[0]
+                pair_splits[split_name] = sets
             with open(self._npz_path, 'wb') as f:
-                np.savez(f, imgs=imgs, names_idx=names_idx, names=names)
+                np.savez(f, imgs=imgs, index=index,
+                         people_splits=people_splits, pair_splits=pair_splits)
             shutil.rmtree(extract_dir)
 
     def _load(self):
         with open(self._npz_path, 'rb') as f:
             dic = np.load(f)
-            return dic['imgs'], dic['names_idx'], dic['names']
+            return (dic['imgs'], dic['index'][()], dic['people_splits'][()],
+                    dic['pair_splits'][()])
