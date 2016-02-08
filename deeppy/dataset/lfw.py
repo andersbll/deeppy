b5798ce7c582eeb48d829b835d79cd45dae88dd5
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2016-02-08 16:06:29 +0100

    add attributes and landmarks to LFW

--- deeppy/dataset/lfw.py
+++ deeppy/dataset/lfw.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import logging
 import numpy as np
+import scipy.io
 from PIL import Image
 from .util import dataset_home, download, checksum, archive_extract, checkpoint
 
@@ -51,6 +52,16 @@ _PEOPLE_SPLIT_URLS = {
     ),
 }
 
+_ATTRIBUTES_URL = (
+    'http://www.cs.columbia.edu/CAVE/databases/pubfig/download/lfw_attributes.txt',
+    'a68c039ac854fd966fe4bdc36674b98f'
+)
+
+_LANDMARKS_URL = (
+    'http://personal.ie.cuhk.edu.hk/~zs014/denseLFW_v2.tar.gz',
+    '44fea754b93a5e678147427b8b4e7ff1'
+)
+
 
 class LFW(object):
     '''
@@ -72,9 +83,41 @@ class LFW(object):
         self.data_dir = os.path.join(dataset_home, self.name)
         self._npz_path = os.path.join(self.data_dir, self.name+'.npz')
         self._install()
-        self.people_splits = self._load()
         (self.imgs, self.index, self.people_splits,
          self.pair_splits) = self._load()
+        self._attributes_path = os.path.join(self.data_dir, 'attributes.npz')
+        self._attributes = None
+        self._attribute_names = None
+        self._landmarks_path = os.path.join(self.data_dir, 'landmarks.npz')
+        self._landmarks68 = None
+        self._landmarks194 = None
+
+    def _download(self, url, md5):
+        log.info('Downloading %s', url)
+        filepath = download(url, self.data_dir)
+        if md5 != checksum(filepath, method='md5'):
+            raise RuntimeError('Checksum mismatch for %s.' % url)
+        return filepath
+
+    @property
+    def attribute_names(self):
+        if self._attribute_names is None:
+            self._load_attributes()
+        return self._attribute_names
+
+    @property
+    def attributes(self):
+        if self._attributes is None:
+            self._load_attributes()
+        return self._attributes
+
+    def landmarks(self, kind='68'):
+        if self._landmarks68 is None:
+            self._load_landmarks()
+        if kind == '68':
+            return self._landmarks68
+        if kind == '194':
+            return self._landmarks194
 
     def _install(self):
         checkpoint_file = os.path.join(self.data_dir, '__install_check')
@@ -82,11 +125,7 @@ class LFW(object):
             if exists:
                 return
             url, md5 = _URLS[self.alignment]
-
-            log.info('Downloading %s', url)
-            filepath = download(url, self.data_dir)
-            if md5 != checksum(filepath, method='md5'):
-                raise RuntimeError('Checksum mismatch for %s.' % url)
+            filepath = self._download(url, md5)
             log.info('Unpacking %s', filepath)
             archive_extract(filepath, self.data_dir)
             extract_dir = os.path.splitext(os.path.split(filepath)[1])[0]
@@ -98,9 +137,9 @@ class LFW(object):
             img_idx = 0
             for person_id in sorted(os.listdir(extract_dir)):
                 person_dir = os.path.join(extract_dir, person_id)
-                img_no = 0
+                img_no = 1
                 for filename in sorted(os.listdir(person_dir)):
-                    if int(filename[-8:-4]) - 1 != img_no:
+                    if int(filename[-8:-4]) != img_no:
                         raise ValueError('Unexpected file: %s' % filename)
                     filepath = os.path.join(person_dir, filename)
                     imgs.append(np.array(Image.open(filepath)))
@@ -114,9 +153,7 @@ class LFW(object):
             log.info('Downloading splits')
             people_splits = {}
             for split_name, (url, md5) in _PEOPLE_SPLIT_URLS.items():
-                filepath = download(url, self.data_dir)
-                if md5 != checksum(filepath, method='md5'):
-                    raise RuntimeError('Checksum mismatch for %s.' % url)
+                filepath = self._download(url, md5)
                 sets = []
                 set_sizes = []
                 current_set = -1
@@ -140,9 +177,7 @@ class LFW(object):
 
             pair_splits = {}
             for split_name, (url, md5) in _PAIR_SPLIT_URLS.items():
-                filepath = download(url, self.data_dir)
-                if md5 != checksum(filepath, method='md5'):
-                    raise RuntimeError('Checksum mismatch for %s.' % url)
+                filepath = self._download(url, md5)
                 sets = []
                 set_sizes = []
                 current_set = -1
@@ -183,3 +218,71 @@ class LFW(object):
             dic = np.load(f)
             return (dic['imgs'], dic['index'][()], dic['people_splits'][()],
                     dic['pair_splits'][()])
+
+    def _install_attributes(self):
+        checkpoint_file = os.path.join(self.data_dir, '__attr_install_check')
+        with checkpoint(checkpoint_file) as exists:
+            if exists:
+                return
+            url, md5 = _ATTRIBUTES_URL
+            filepath = self._download(url, md5)
+
+            attributes = {}
+            with open(filepath, 'r') as f:
+                line = f.readline()
+                line = f.readline()
+                fields = [s.strip() for s in line.split('\t')]
+                attribute_names = [s.strip() for s in line.split('\t')[3:]]
+                i = 0
+                for line in f:
+                    i += 1
+                    fields = [s.strip() for s in line.split('\t')]
+                    person_id = fields[0].replace(' ', '_')
+                    img_no = int(fields[1].replace(' ', '_'))
+                    attr_vec = np.array([float(s) for s in fields[2:]])
+                    if person_id not in attributes:
+                        attributes[person_id] = {}
+                    attributes[person_id][img_no] = attr_vec
+
+            with open(self._attributes_path, 'wb') as f:
+                np.savez(f, attributes=attributes,
+                         attribute_names=attribute_names)
+
+    def _load_attributes(self):
+        self._install_attributes()
+        with open(self._attributes_path, 'rb') as f:
+            dic = np.load(f)
+            self._attributes = dic['attributes'][()]
+            self._attribute_names = dic['attribute_names'][()]
+
+    def _install_landmarks(self):
+        checkpoint_file = os.path.join(self.data_dir, '__landm_install_check')
+        with checkpoint(checkpoint_file) as exists:
+            if exists:
+                return
+            url, md5 = _LANDMARKS_URL
+            filepath = self._download(url, md5)
+            log.info('Unpacking %s', filepath)
+            archive_extract(filepath, self.data_dir)
+            mat_path = os.path.join(self.data_dir, 'denseLFW_v2.mat')
+            matdict = scipy.io.loadmat(mat_path)
+            img_paths = [str(s[0][0]) for s in matdict['nameList']]
+            landmarks68 = np.zeros_like(matdict['pose68'])
+            landmarks194 = np.zeros_like(matdict['pose194'])
+            for i, img_path in enumerate(img_paths):
+                person_id = img_path.split('/')[0]
+                if person_id not in self.index.keys():
+                    raise ValueError('invalid person_id')
+                img_no = int(img_path[-8:-4])
+                img_idx = self.index[person_id][img_no-1]
+                landmarks68[img_idx] = matdict['pose68'][i]
+                landmarks194[img_idx] = matdict['pose194'][i]
+            with open(self._landmarks_path, 'wb') as f:
+                np.savez(f, landmarks68=landmarks68, landmarks194=landmarks194)
+
+    def _load_landmarks(self):
+        self._install_landmarks()
+        with open(self._landmarks_path, 'rb') as f:
+            dic = np.load(f)
+            self._landmarks68 = dic['landmarks68']
+            self._landmarks194 = dic['landmarks194']
