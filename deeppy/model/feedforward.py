2635737ad66d3d40609351acda0e9f5f72870c4a
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2016-02-10 18:10:22 +0100

    revise FeedForwardNet

--- deeppy/model/feedforward.py
+++ deeppy/model/feedforward.py
@@ -1,11 +1,7 @@
 import numpy as np
-import cudarray as ca
 from ..base import Model, CollectionMixin, PickleMixin
 from ..input import Input
-from ..loss import SoftmaxCrossEntropy
-from ..expr import nnet
-from ..expr.base import Source
-from ..expr.graph import ExprGraph
+from .. import expr
 
 
 class FeedForwardNet(Model, CollectionMixin, PickleMixin):
@@ -18,59 +14,66 @@ class FeedForwardNet(Model, CollectionMixin, PickleMixin):
         self.collection = [expression]
 
     def setup(self, x_shape, y_shape=None):
-        self._x_src = Source(x_shape)
-        self._y_src = Source(y_shape)
-        y_pred = self.expression(self._x_src)
-        loss = self.loss(y_pred, self._y_src)
-        self._graph = ExprGraph(loss)
+        self._x_src = expr.Source(x_shape)
+        y_expr = self._fprop_expr(self._x_src)
+        if y_shape is not None:
+            self._y_src = expr.Source(y_shape)
+            y_expr = self.loss(y_expr, self._y_src)
+        self._graph = expr.ExprGraph(y_expr)
         self._graph.setup()
-        self._graph.out_grad = ca.array(1)
+        self._graph.out_grad = 1.0
+
+    def _fprop_expr(self, x):
+        return self.expression(x)
 
     def update(self, x, y):
         self._x_src.out = x
         self._y_src.out = y
         self._graph.fprop()
         self._graph.bprop()
-        y_pred = self._graph.out
-        return self.loss.loss(y_pred, y)
+        return self.loss.out
+
+    def _batchwise(self, input, expr_fun):
+        input = Input.from_any(input)
+        src = expr.Source(input.x_shape)
+        graph = expr.ExprGraph(expr_fun(src))
+        graph.setup()
+        y = []
+        for batch in input.batches():
+            src.out = batch['x']
+            graph.fprop()
+            y.append(np.array(graph.out))
+        y = np.concatenate(y)[:input.n_samples]
+        return y
+
+    def predict(self, input):
+        """ Calculate the output for the given input. """
+        return self._batchwise(input, self._fprop_expr)
 
 
 class ClassifierNet(FeedForwardNet):
-    def fprop(self, x):
-        self._src.out = x
-        self._graph.fprop()
-        y = self._graph.out
-        y = nnet.softmax(y)
-        return y
+    def _fprop_expr(self, x):
+        y_expr = super(ClassifierNet, self)._fprop_expr(x)
+        if isinstance(self.loss, expr.nnet.SoftmaxCrossEntropy):
+            softmax = expr.nnet.SoftmaxCrossEntropy.SoftmaxIdentityBProp()
+            y_expr = softmax(y_expr)
+        return y_expr
 
-    def _predict(self, input, top_layers):
-        input = Input.from_any(input)
-        input.reset()
-        self.phase = 'test'
-        self.layers += top_layers
-        y = []
-        for batch in input.batches():
-            x_batch = batch['x']
-            y_batch = np.array(self.fprop(x_batch))
-            y.append(y_batch)
-        y = np.concatenate(y)[:input.n_samples]
-        self.layers = self.layers[:len(self.layers)-len(top_layers)]
-        return y
+    def _predict_expr(self, x):
+        y_expr = self._fprop_expr(x)
+        y_expr = expr.nnet.one_hot.OneHotDecode()(y_expr)
+        return y_expr
 
     def predict(self, input):
         """ Calculate the output for the given input. """
-        top_layers = []
-        if isinstance(self.loss, SoftmaxCrossEntropy):
-            # Add softmax from SoftmaxCrossEntropy
-            top_layers.append(self.loss)
-        return self._predict(input, top_layers)
+        return self._batchwise(input, self._predict_expr)
 
     def predict_proba(self, input):
         """ Calculate the output probabilities for the given input. """
-        return self._predict(input, [])
+        return self._batchwise(input, self._fprop_expr)
 
 
-class RegressorNet(ClassifierNet):
+class RegressorNet(FeedForwardNet):
     def predict(self, input):
         """ Calculate the output for the given input. """
-        return self._predict(input, [])
+        return self._batchwise(input, self._fprop_expr)
