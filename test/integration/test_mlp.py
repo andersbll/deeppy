eaa18ec8b9b9f903902a6beb3ea592dfd120f4b0
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2016-02-16 10:41:20 +0100

    Update to revised train module

--- test/integration/test_mlp.py
+++ test/integration/test_mlp.py
@@ -12,26 +12,21 @@ def test_classification():
         n_samples=n_samples, n_features=n_features, n_classes=n_classes,
         n_informative=n_classes*2, random_state=1
     )
-
+    x = x.astype(dp.float_)
+    y = y.astype(dp.int_)
     n_train = int(0.8 * n_samples)
-    n_val = int(0.5 * (n_samples - n_train))
-
     x_train = x[:n_train]
     y_train = y[:n_train]
-    x_val = x[n_train:n_train+n_val]
-    y_val = y[n_train:n_train+n_val]
-    x_test = x[n_train+n_val:]
-    y_test = y[n_train+n_val:]
+    x_test = x[n_train:]
+    y_test = y[n_train:]
 
     scaler = dp.StandardScaler()
     x_train = scaler.fit_transform(x_train)
-    x_val = scaler.transform(x_val)
     x_test = scaler.transform(x_test)
 
     # Setup input
     batch_size = 16
     train_input = dp.SupervisedInput(x_train, y_train, batch_size=batch_size)
-    val_input = dp.Input(x_val)
     test_input = dp.Input(x_test)
 
     # Setup neural network
@@ -59,12 +54,9 @@ def test_classification():
     )
 
     # Train neural network
-    def val_error():
-        return np.mean(net.predict(val_input) != y_val)
-    trainer = dp.GradientDescent(
-        min_epochs=10, learn_rule=dp.Momentum(learn_rate=0.01, momentum=0.9),
-    )
-    trainer.train(net, train_input, val_error)
+    learn_rule = dp.Momentum(learn_rate=0.01/batch_size, momentum=0.9)
+    trainer = dp.GradientDescent(net, train_input, learn_rule)
+    trainer.train_epochs(n_epochs=10)
 
     # Evaluate on test data
     error = np.mean(net.predict(test_input) != y_test)
