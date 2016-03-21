3f63c2855060943f4c368e6eb5de9413d2a138ec
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2016-03-21 11:47:11 +0100

    revise Loss

--- deeppy/expr/nnet/loss.py
+++ deeppy/expr/nnet/loss.py
@@ -1,34 +1,41 @@
-import numpy as np
 import cudarray as ca
 from ..base import Op
 from .activation import Softmax
 from .one_hot import OneHot
-_FLT_MIN = np.finfo(ca.float_).tiny
 
 
 class Loss(Op):
+    axis = None
+    bcast_shape = None
+
     def __call__(self, pred, target):
         self.pred = pred
         self.target = target
         self.inputs = [pred, target]
         return self
 
-    def setup(self):
-        self.shape = (self.pred.shape[0], 1)
+    def setup_from_shape(self, in_shape):
+        batch_size = in_shape[0]
+        self.shape = (batch_size,)
         self.array = ca.zeros(self.shape)
         self.grad_array = ca.ones(self.shape)
+        self.axis = tuple(range(1, len(in_shape)))
+        self.bcast_shape = tuple([batch_size, ] + [1, ]*len(self.axis))
+
+    def setup(self):
+        self.setup_from_shape(self.pred.shape)
 
 
 class SquareError(Loss):
     def fprop(self):
         diff = self.pred.array - self.target.array
         diff **= 2.0
-        ca.sum(diff, axis=1, keepdims=True, out=self.array)
+        ca.sum(diff, axis=self.axis, out=self.array)
 
     def bprop(self):
         ca.subtract(self.pred.array, self.target.array, self.pred.grad_array)
         self.pred.grad_array *= 2
-        self.pred.grad_array *= self.grad_array
+        self.pred.grad_array *= ca.reshape(self.grad_array, self.bcast_shape)
 
 
 class BinaryCrossEntropy(Loss):
@@ -47,7 +54,7 @@ class BinaryCrossEntropy(Loss):
         tmp2 *= self.target.array
         ca.add(tmp1, tmp2, tmp1)
         tmp1 *= -1
-        ca.sum(tmp1, axis=1, keepdims=True, out=self.array)
+        ca.sum(tmp1, axis=1, out=self.array)
 
     def bprop(self):
         # -(target/pred - (1 - target)/(1 - pred))
@@ -58,27 +65,36 @@ class BinaryCrossEntropy(Loss):
         ca.add(self.pred.array, self.eps, tmp2)
         ca.divide(self.target.array, tmp2, out=tmp2)
         ca.subtract(tmp1, tmp2, self.pred.grad_array)
-        self.pred.grad_array *= self.grad_array
+        self.pred.grad_array *= ca.reshape(self.grad_array, self.bcast_shape)
+
+
+class _SoftmaxIdentityBProp(Softmax):
+    def bprop(self):
+        self.x.grad_array = self.grad_array
 
 
 class SoftmaxCrossEntropy(Loss):
-    class SoftmaxIdentityBProp(Softmax):
-        def bprop(self):
-            self.x.grad_array = self.grad_array
-
-    def __init__(self, n_classes, one_hot_targets=True):
+    def __init__(self, n_classes=None, eps=1e-8):
         self.n_classes = n_classes
-        self.one_hot_targets = one_hot_targets
+        self.eps = eps
 
     def __call__(self, pred, target):
-        if self.one_hot_targets:
+        if isinstance(pred, Softmax) and \
+           not isinstance(pred, _SoftmaxIdentityBProp):
+            pred = pred.x
+        pred = _SoftmaxIdentityBProp()(pred)
+        if self.n_classes is not None:
             target = OneHot(self.n_classes)(target)
         return super(SoftmaxCrossEntropy, self).__call__(pred, target)
 
     def fprop(self):
-        self.array = ca.nnet.categorical_cross_entropy(
-            y_pred=self.pred.array, y_true=self.target.array
-        )
+        # -target * log(pred)
+        tmp1 = self.pred.array + self.eps
+        ca.log(tmp1, tmp1)
+        tmp1 *= self.target.array
+        ca.sum(tmp1, axis=1, out=self.array)
+        ca.negative(self.array, self.array)
 
     def bprop(self):
-        self.pred.grad_array = -(self.target.array - self.pred.array)
+        ca.subtract(self.pred.array, self.target.array, self.pred.grad_array)
+        self.pred.grad_array *= ca.reshape(self.grad_array, self.bcast_shape)
