06165b9e6a48f43677cedb68c91fecd6394c1429
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2016-04-06 10:42:00 +0200

    more efficient implementation

--- deeppy/train/learn_rules.py
+++ deeppy/train/learn_rules.py
@@ -30,32 +30,35 @@ class Momentum(LearnRule):
 
 
 class RMSProp(LearnRule):
-    def __init__(self, learn_rate=0.1, decay=0.9, eps=1e-8):
+    def __init__(self, learn_rate=0.001, decay=0.9, eps=1e-8):
         self.learn_rate = learn_rate
         self.decay = decay
         self.eps = eps
 
     def init_state(self, param):
-        last_step = ca.zeros_like(param.grad_array)
-        return last_step
+        mean_square = ca.zeros_like(param.grad_array)
+        return mean_square
 
-    def step(self, param, last_step):
-        last_step *= self.decay
-        step = param.grad()
-        last_step += (1.0 - self.decay) * step**2
-        scaling = ca.sqrt(last_step) + self.eps
-        step *= -self.learn_rate
-        step /= scaling
-        param.step(step)
+    def step(self, param, mean_square):
+        grad = param.grad()
+        # mean_square = decay*mean_square + (1 - decay)*grad
+        mean_square *= self.decay
+        tmp = grad**2
+        tmp *= (1 - self.decay)
+        mean_square += tmp
+        # step = -learn_rate*grad/(sqrt(mean_square) + eps)
+        ca.sqrt(mean_square, tmp)
+        tmp += self.eps
+        ca.divide(grad, tmp, tmp)
+        tmp *= -self.learn_rate
+        param.step(tmp)
 
 
 class Adam(LearnRule):
-    def __init__(self, learn_rate=0.1, beta1=0.9, beta2=0.999, lambd=1-1e-8,
-                 eps=1e-8):
+    def __init__(self, learn_rate=0.001, beta1=0.9, beta2=0.999, eps=1e-8):
         self.learn_rate = learn_rate
         self.beta1 = beta1
         self.beta2 = beta2
-        self.lambd = lambd
         self.eps = eps
 
     def init_state(self, param):
@@ -69,13 +72,20 @@ class Adam(LearnRule):
         grad = param.grad()
         t += 1
         t = int(t)
-        beta1_t = self.beta1 * self.lambd**(t - 1)
-        m *= beta1_t
-        m += (1 - beta1_t) * grad
+        # m = beta1*m + (1 - beta1)*grad
+        m *= self.beta1
+        tmp = (1 - self.beta1)*grad
+        m += tmp
+        # v = beta2*v + (1 - beta2)*grad**2
         v *= self.beta2
-        v += (1 - self.beta2) * grad**2
-        learn_rate = (self.learn_rate * (1 - self.beta2**t)**0.5 /
-                      (1 - self.beta1**t))
-        step = m / (ca.sqrt(v) + self.eps)
-        step *= -learn_rate
-        param.step(step)
+        ca.power(grad, 2, tmp)
+        tmp *= (1 - self.beta2)
+        v += tmp
+        # alpha = learn_rate*sqrt(1 - beta2**t)/(1 - beta1**t)
+        # step = -alpha_t*m/(sqrt(v) + eps)
+        alpha = self.learn_rate*np.sqrt(1 - self.beta2**t)/(1 - self.beta1**t)
+        ca.sqrt(v, tmp)
+        tmp += self.eps
+        ca.divide(m, tmp, tmp)
+        tmp *= -alpha
+        param.step(tmp)
